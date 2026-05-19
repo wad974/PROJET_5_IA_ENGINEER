eb5ca13d80b411cd40bd05ba733653a59785e547
wad974 <woodartdeco974@gmail.com>
2026-05-19 19:13:07 +0200

    ajout tous les pip plus environnement

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
