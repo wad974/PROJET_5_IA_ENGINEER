bab78502262da064e586b6da6ac331b5e3bf0b57
jcwad <woodartdeco974@gmail.com>
2026-05-28 20:07:21 +0400

    ajout ligne dans data pour test_train_model

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -9,6 +9,8 @@ def test_train_model():
         'id_employe': [1, 2, 3, 4, 5],
         'age': [25, 30, 35, 40, 45],
         'salaire': [30000, 40000, 50000, 60000, 70000],
+        'nombre_employee_sous_responsabilite': [0, 1, 2, 3, 4],
+        'nombre_heures_travailless': [0, 1, 2, 3, 4],
         'a_quitte_l_entreprise_Oui': [0, 0, 1, 1, 0]
     }
     df_fictif = pd.DataFrame(data)
